dbc8fee105dd22bfb234e840379b0945ee436fee
davawen <arthaudawen.26@gmail.com>
2021-09-22 20:44:54 +0200

    Add basic commands to interpretation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <vector>
 #include <exception>
@@ -37,14 +38,72 @@ int main(int argc, char *argv[])
 	parseInstructions(argv[1], instructions);
 	
 	printf("\n[STARTING EXECUTION FROM HERE]\n\n");
+
+	constexpr int STACK_SIZE = 10000;
+	int stack[STACK_SIZE];
+	int *sp = stack;
 	
-	for(auto &it : instructions)
+	// Register is apparently a reserved c++ keyword *shrugs*
+	int reg = 0;
+
+	auto fatalErr = [](const char *str) -> void
 	{
+		printf("\x1b[31mFatal error: %s.\n", str);
+	};
+
+	auto warningErr = [](const char *str) -> void
+	{
+		printf("\x1b[41mWarning: %s.\n", str);
+	};
+	
+	for(auto iterator = instructions.begin(); iterator != instructions.end(); iterator++)
+	{
+		auto &it = *iterator;
+
+		// Error checking
+		if(sp < stack)
+		{
+			// TODO: Add flavor text (... at line xx and instruction xx)
+			fatalErr("Stack underflow");
+		}
+
+		if(sp > stack + STACK_SIZE)
+		{
+			warningErr("Stack overflow, probable segmentation fault");
+		}
 		// std::cout << it << '\n';
 		
 		switch(it.type)
 		{
+			case Instruction::Type::push:
+				if(it.numValues == 0)
+				{
+					// Push register value instead
+					sp++;
+					*sp = reg;
+				}
+				for(int i = 0; i < it.numValues; i++)
+				{
+					sp++;	
+					*sp = it.value[i];
+				}
+				break;
+			case Instruction::Type::pop:
+				// Pop current stack value to register
+				reg = *sp;
+				sp--;
+				break;
+			case Instruction::Type::dup:
+				// Duplicates the value of top of the stack
+				sp++;
+				*sp = *(sp-1);
+				break;
 			case Instruction::Type::print:
+				if(it.numValues == 0) // Print register
+				{
+					printf("%i\n", reg);
+				}
+
 				for(int i = 0; i < it.numValues; i++)
 				{
 					printf("%s", reinterpret_cast<char *>(it.value[i]));
@@ -55,8 +114,8 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	// int stack[10000];
-	// int *sp = stack;
+	
+	
 	
 	
 	
